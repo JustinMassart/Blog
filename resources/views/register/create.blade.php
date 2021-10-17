96115490690342f6282b736976e6bcb8be6b31e5
JustinMassart <justin.massart@student.hepl.be>
2021-10-17 20:21:45 +0200

    commit changes

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,55 +1,36 @@
 <x-layout>
-
     <x-slot name="mainContent">
-
         <sectiosn class="px-6 py-8">
-
-            <form action="/register" method="POST">
-
-                <div>
-
-
-                    <label for="email">Email</label>
-
-                    <input class="border" type="email" id="email" name="email" value="">
-
-                </div>
-
-                <div>
-
-                    <label for="name">Name</label>
-
-                    <input class="border" type="text" id="name" name="name" value="">
-
-
-                </div>
-
-                <div>
-
-                    <label for="username">Username</label>
-
-                    <input class="border" type="text" id="username" name="username" value="">
-
-
-                </div>
-
-                <div>
-
-                    <label for="password">Password</label>
-
-                    <input class="border" type="password" id="password" name="password" value="">
-
-
-                </div>
-
-                <input type="submit" value="Register">
-
-
-            </form>
-
-
+            <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
+                <h1 class="text-center font-bold text-xl">Register</h1>
+                <form action="/register" method="POST" class="mt-10">
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">Email</label>
+                        <input class="border border-gray-400 p-2 w-full" type="email" id="email" name="email" value="">
+                    </div>
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">Name</label>
+                        <input class="border border-gray-400 p-2 w-full" type="text" id="name" name="name" value="">
+                    </div>
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="username">Username</label>
+                        <input class="border border-gray-400 p-2 w-full" type="text" id="username" name="username"
+                               value="">
+                    </div>
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="password">Password</label>
+                        <input class="border border-gray-400 p-2 w-full" type="password" id="password" name="password"
+                               value="" required>
+                    </div>
+                    <div class="mb-6">
+                        <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </main>
         </sectiosn>
-
     </x-slot>
-
 </x-layout>
